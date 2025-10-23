90974349e5ba693df32b27e37759afdc16771598
kahitsino <tolentinoaaron200@gmail.com>
2025-10-23 02:29:57 +0000

    python calcu done, with error handling and zero division error

--- calcu/calcu.py
+++ calcu/calcu.py
@@ -5,6 +5,11 @@ invalid_number = 'System: Invalid number'
 
 while True:
     print('=== CALCULATOR ===')
+    print('1. Addition')
+    print('2. Subtration')
+    print('3. Multiplication')
+    print('4. Division')
+    print('5. Exit')
     try:
         UserInput = int(input('Enter a number: '))
     except ValueError:
@@ -19,16 +24,31 @@ while True:
         except ValueError:
             print(invalid_number)
     elif UserInput == 2:
-        subtraction1 = int(input(first_number))
-        subtraction2 = int(input(second_number))
-        print('Answer: ', subtraction1 - subtraction2)
+        try:
+            subtraction1 = int(input(first_number))
+            subtraction2 = int(input(second_number))
+            print('Answer: ', subtraction1 - subtraction2)
+        except ValueError:
+            print(invalid_number)
     elif UserInput == 3:
-        multiplication1 = int(input(first_number))
-        multiplication2 = int(input(second_number))
-        print('Answer: ', multiplication1 * multiplication2)
+        try:
+            multiplication1 = int(input(first_number))
+            multiplication2 = int(input(second_number))
+            print('Answer: ', multiplication1 * multiplication2)
+        except ValueError:
+            print(invalid_number)
     elif UserInput == 4:
-        divition1 = int(input(first_number))
-        divition2 = int(input(second_number))
-        print('Answer: ', divition1 / divition2)
+        try:
+            division1 = int(input(first_number))
+            division2 = int(input(second_number))
+            if division2 == 0:
+                print(division1, ' cant divide in ', division2)
+            else:
+                print('Answer: ', division1 / division2)
+        except ValueError:
+            print(invalid_number)
     elif UserInput == 5:
         print('System: Exited!')
+        break
+    else:
+        pass
